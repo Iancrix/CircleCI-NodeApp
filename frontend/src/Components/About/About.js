84fbb750a166fd68ce894c59bd2b878074d6f5fa
Ian Chona <ianchonaramirez@gmail.com>
2020-10-23 23:56:00 -0500

    lol

--- frontend/src/Components/About/About.js
+++ frontend/src/Components/About/About.js
@@ -4,7 +4,7 @@ import "./About.css";
 export default function About() {
 	return (
 		<div className="about">
-			<h1 className="autor">Made by Ian Chona ALOOOOOO!!!!!!</h1>
+			<h1 className="autor">Made by Ian Chona owowowow</h1>
 		</div>
 	);
 }
